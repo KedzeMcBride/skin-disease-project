51c8f8f5cd544a88db9c5ebb5de39746ad95be87
KedzeMcbride <Kedzemcbridekum@gmail.com>
2025-04-14 15:52:07 +0100

    registration validation

--- project-diagnog-frontend/src/App.jsx
+++ project-diagnog-frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timeout = setTimeout(() => setLoading(false), 1500);
+    const timeout = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timeout);
   }, []);
 
